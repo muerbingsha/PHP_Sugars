6386baeb75afec3dcb6ab006176d84d2b2989061
muerbingsha <511071007@qq.com>
2018-06-26 11:53:20 +0800

    Create funtion2_test.php

---
+++ Extension/1_function-class/function2/funtion2_test.php
@@ -0,0 +1,6 @@
+var_dump(myDefault("int"));
+var_dump(myDefault("int", 1));
+var_dump(myDefault("bool"));
+var_dump(myDefault("bool", true));
+var_dump(myDefault("str","A"));
+var_dump(myDefault("str"));
